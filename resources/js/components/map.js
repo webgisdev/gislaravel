a6a31bc780de6f2f967dbd96f94abe638022a04b
LaravelGIS <etienne@laravelgis.com>
2022-12-13 21:47:03 -0500

    layer-opacity-and-info-popup-in-openlayers

--- resources/js/components/map.js
+++ resources/js/components/map.js
@@ -6,55 +6,105 @@ import VectorLayer from "ol/layer/Vector";
 import OSM from "ol/source/OSM.js";
 import { Style, Fill, Stroke, Circle, Text } from "ol/style.js";
 import GeoJSON from "ol/format/GeoJSON";
+import Overlay from "ol/Overlay.js";
 
 document.addEventListener("alpine:init", () => {
     Alpine.data("map", function () {
         return {
             legendOpened: false,
             map: {},
-            monumentsLayer: [],
             initComponent() {
                 const paramsObj = {
-                    servive: 'WFS',
-                    version: '2.0.0',
-                    request: 'GetFeature',
-                    typeName: 'laravelgis:monuments',
-                    outputFormat: 'application/json',
-                    crs: 'EPSG:4326',
-                    srsName: 'EPSG:4326',
-                }
+                    servive: "WFS",
+                    version: "2.0.0",
+                    request: "GetFeature",
+                    typeName: "laravelgis:monuments",
+                    outputFormat: "application/json",
+                    crs: "EPSG:4326",
+                    srsName: "EPSG:4326",
+                };
 
-                const urlParams = new URLSearchParams(paramsObj)
-                const monumentsUrl = 'http://localhost:8080/geoserver/wfs?' + urlParams.toString()
-
-                this.monumentsLayer = new VectorLayer({
-                    source: new VectorSource({
-                        format: new GeoJSON(),
-                        url: monumentsUrl,
-                    }),
-                    style: this.styleFunction,
-                    label: "Monuments",
-                })
+                const urlParams = new URLSearchParams(paramsObj);
+                const monumentsUrl =
+                    "http://localhost:8080/geoserver/wfs?" +
+                    urlParams.toString();
 
                 this.map = new Map({
                     target: this.$refs.map,
                     layers: [
                         new TileLayer({
                             source: new OSM(),
-                            label: "OpenStreetMap",
+                            label: 'OpenStreetMap',
                         }),
-                        this.monumentsLayer,
+                        new VectorLayer({
+                            source: new VectorSource({
+                                format: new GeoJSON(),
+                                url: monumentsUrl,
+                            }),
+                            style: this.styleFunction,
+                            label: 'Monuments',
+                        }),
                     ],
                     view: new View({
                         projection: "EPSG:4326",
                         center: [0, 0],
                         zoom: 2,
                     }),
+                    overlays: [
+                        new Overlay({
+                            id: 'info',
+                            element: this.$refs.popup,
+                            stopEvent: true,
+                        }),
+                    ],
                 });
 
+                this.map.on("singleclick", (event) => {
+                    if (event.dragging) {
+                        return;
+                    }
+
+                    let overlay = this.map.getOverlayById('info')
+                    overlay.setPosition(undefined)
+                    this.$refs.popupContent.innerHTML = ''
+
+                    this.map.forEachFeatureAtPixel(
+                        event.pixel,
+                        (feature, layer) => {
+                            if (layer.get('label') === 'Monuments' && feature) {
+                                this.gotoFeature(feature)
+
+                                let content =
+                                    '<h4 class="text-gray-500 font-bold">' +
+                                    feature.get('name') +
+                                    '</h4>'
+
+                                content +=
+                                    '<img src="' +
+                                    feature.get('image') +
+                                    '" class="mt-2 w-full max-h-[200px] rounded-md shadow-md object-contain overflow-clip">'
+
+                                this.$refs.popupContent.innerHTML = content
+
+                                setTimeout(() => {
+                                    overlay.setPosition(
+                                        feature.getGeometry().getCoordinates()
+                                    );
+                                }, 500)
+
+                                return
+                            }
+                        },
+                        {
+                            hitTolerance: 5,
+                        }
+                    );
+                });
             },
-            get monumentsFeatures() {
-                return this.monumentsLayer.getSource().getFeatures()
+            closePopup() {
+                let overlay = this.map.getOverlayById('info')
+                overlay.setPosition(undefined)
+                this.$refs.popupContent.innerHTML = ''
             },
             styleFunction(feature, resolution) {
                 return new Style({
@@ -87,8 +137,8 @@ document.addEventListener("alpine:init", () => {
             gotoFeature(feature) {
                 this.map.getView().animate({
                     center: feature.getGeometry().getCoordinates(),
-                    zoom: 10,
-                    duration: 2000,
+                    zoom: 15,
+                    duration: 500,
                 });
             },
         };
--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -3,52 +3,54 @@
         <div class="absolute top-2 right-8 z-10 rounded-md bg-white bg-opacity-75">
             <div class="ol-unselectable ol-control">
                 <button x-on:click.prevent="legendOpened = ! legendOpened" title="Open/Close legend"
-                        class="absolute inset-0 flex items-center justify-center">
+                    class="absolute inset-0 flex items-center justify-center">
                     <!-- Heroicon name: outline/globe -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 pl-0.5" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor" stroke-width="1">
+                        stroke="currentColor" stroke-width="1">
                         <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                 </button>
             </div>
         </div>
+
         <div x-cloak x-show="legendOpened" x-transition:enter="transition-opacity duration-300"
-             x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
-             x-transition:leave="transition-opacity duration-300" x-transition:leave-start="opacity-100"
-             x-transition:leave-end="opacity-0"
-             class="absolute right-0 top-16 left-2 bottom-2 z-10 max-w-sm rounded-md border border-slate-300 bg-white bg-opacity-50 shadow-sm">
+            x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+            x-transition:leave="transition-opacity duration-300" x-transition:leave-start="opacity-100"
+            x-transition:leave-end="opacity-0"
+            class="absolute right-0 top-16 left-2 bottom-2 z-10 max-w-sm rounded-md border border-slate-300 bg-white bg-opacity-50 shadow-sm">
             <div class="absolute inset-1 rounded-md bg-white bg-opacity-75 p-2">
                 <div class="flex items-start justify-between">
                     <h3 class="text-lg font-medium text-slate-700">Legend</h3>
                     <button x-on:click.prevent="legendOpened = false"
-                            class="text-2xl font-black text-slate-400 transition hover:text-[#3369A1] focus:text-[#3369A1] focus:outline-none">&times;</button>
+                        class="text-2xl font-black text-slate-400 transition hover:text-[#3369A1] focus:text-[#3369A1] focus:outline-none">&times;</button>
                 </div>
                 <ul class="mt-2 space-y-1 rounded-md border border-slate-300 bg-white p-2">
                     <template x-for="(layer, index) in map.getAllLayers().reverse()" :key="index">
                         <li class="flex items-center px-2 py-1">
-                            <div x-id="['legend-checkbox']">
-                                <label x-bind:for="$id('legend-checkbox')" class="flex items-center">
-                                    <input type="checkbox" x-bind:checked="layer.getVisible()"
-                                           x-bind:id="$id('legend-checkbox')"
-                                           x-on:change="layer.setVisible(!layer.getVisible())"
-                                           class="rounded border-slate-300 text-[#3369A1] shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-                                    <span class="ml-2 text-sm text-slate-600" x-text="layer.get('label')"></span>
+                            <div x-id="['legend-range']" class="w-full">
+                                <label x-bind:for="$id('legend-range')" class="flex items-center">
+                                    <span class="text-sm text-slate-600" x-text="layer.get('label')"></span>
                                 </label>
-                                <template x-if="layer.get('label') === 'Monuments' && layer.getVisible()">
-                                    <div class="mt-2 ml-6 text-sm text-slate-600">
-                                        <template x-for="(feature, index) in monumentsFeatures" :key="index">
-                                            <a href="#" :title="'Go to ' + feature.get('name')"
-                                               x-text="feature.get('name')" x-on:click.prevent="gotoFeature(feature)"
-                                               class="block transition hover:text-slate-800 hover:underline focus:text-slate-800 focus:underline focus:outline-none">
-                                            </a>
-                                        </template>
-                                    </div>
-                                </template>
+                                <div class="mt-1 text-sm text-slate-600">
+                                    <input class="w-full accent-[#3369A1]" type="range" min="0" max="1" step="0.01"
+                                        x-bind:id="$id('legend-range')" x-bind:value="layer.getOpacity()"
+                                        x-on:change="layer.setOpacity(Number($event.target.value))">
+                                </div>
                             </div>
                         </li>
                     </template>
                 </ul>
+            </div>
+        </div>
+        <div x-cloak x-ref="popup" class="ol-popup ol-control transition">
+            <div class="p-2 m-0.5 bg-white rounded-md">
+                <div class="flex justify-between">
+                    <h3 class="text-xs font-medium text-slate-400">Monument</h3>
+                    <a href="#" title="Close" x-on:click.prevent="closePopup"
+                        class="-mt-1 font-black text-slate-400 transition hover:text-slate-600 focus:text-slate-600 focus:outline-none">&times;</a>
+                </div>
+                <div x-ref="popupContent" class="mt-2 overflow-y-auto min-h-[200px]"></div>
             </div>
         </div>
     </div>
